9c77bb7f4bc11d02dcf83c03bcbccb568cf78ed1
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-05-31 15:15:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void CreateStart()
+        {
+            byte fuel;
+            sbyte serialNumber;
+            int age;
+            uint celsius;
+            decimal money;
+            float solution;
+            bool check;
+            short litr;
+            ushort nameOfDoor;
+            double farengeit;
+        }
+    }
+}
