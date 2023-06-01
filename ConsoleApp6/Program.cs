612adf84be70eb7311c236c0d64ae37afd153664
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-01 10:28:16 +0300

    3

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Грозный";
+            string secondName = "Иван";
+            Console.WriteLine($"Так неправильно {name} {secondName}");
+            name = "Иван";
+            secondName = "Грозный";
+            Console.WriteLine($"Так правильно {name} {secondName}");
         }
     }
 }
--- KtoTiVoin/Program.cs
+++ KtoTiVoin/Program.cs
@@ -8,13 +8,6 @@
             int pictureOst = 52 % 3;
             Console.WriteLine($"Заполнено рядов {pictureFull}");
             Console.WriteLine($"Остаток {pictureOst}");
-
-            string name = "Грозный";
-            string secondName = "Иван";
-            Console.WriteLine($"Так неправильно {name} {secondName}");
-            name = "Иван";
-            secondName = "Грозный";
-            Console.WriteLine($"Так правильно {name} {secondName}");
         }
     }
 }
