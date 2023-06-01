d2000715a9be2dd9be8a263429cf9d29a0c8a570
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-01 15:56:56 +0300

    98

---
+++ Hospital/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int waiting = 10;
+            Console.WriteLine("Рассчет времени просиживания штанов");
+            Console.WriteLine("Сколько перед Вами человек?");
+            var person = Console.ReadLine();
+            var generalTime = Convert.ToInt64(person) * waiting;
+            var timeMinute = generalTime % 60;
+            var timeHour = generalTime / 60;
+            Console.WriteLine($"Осталось ждать: {timeHour} час {timeMinute} минут ");
+        }
+    }
+}
