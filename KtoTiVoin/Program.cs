4ecbc98e69c7aaf0b00df2b84123e1892a5cbe7f
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-01 09:39:12 +0300

    55

--- KtoTiVoin/Program.cs
+++ KtoTiVoin/Program.cs
@@ -4,15 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Кто ты воин?");
-            string name = Console.ReadLine();
-            Console.WriteLine($"{name}? Я помню это имя. Правитель Фессалии должен носить скипетр.Отдай его своему царю в знак смирения.");
-            Console.WriteLine($"Голос в голове и шило в попе: - Что ответишь этому безумцу?");
-            string voice = Console.ReadLine();
-            Console.WriteLine($"{voice} - ответ неверный.Слава Менелаю, нашему великому царю!");
-            Console.WriteLine($"{name}, напиши любое значение для вручения почетной грамоты!");
-            string thankYou = Console.ReadLine();
-            Console.WriteLine($"Грамота - Спасибо тебе воин, {name}!");
+            int pictureFull = 52 / 3;
+            int pictureOst = 52 % 3;
+            Console.WriteLine($"Заполнено рядов {pictureFull}");
+            Console.WriteLine($"Остаток - {pictureOst}");
         }
     }
 }
