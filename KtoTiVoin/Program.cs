899f214e6fc08002e21803e9095e359517ba20da
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-01 10:03:14 +0300

    у

--- KtoTiVoin/Program.cs
+++ KtoTiVoin/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             int pictureFull = 52 / 3;
-            int pictureOst = 52 % 3;
+            double pictureOst = 52.0 % 3.0;
             Console.WriteLine($"Заполнено рядов {pictureFull}");
-            Console.WriteLine($"Остаток - {pictureOst}");
+            Console.WriteLine($"Остаток {pictureOst}");
         }
     }
 }
