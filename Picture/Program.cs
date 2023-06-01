2f5409f22ae05f386bfe68e9603c0b1d18f55e4d
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-01 09:33:23 +0300

    hn

---
+++ Picture/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Picture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pictureFull = 52 / 3;
+            int pictureOst = 52 % 3;
+            Console.WriteLine($"Столько рядов будет заполнено полностью {pictureFull}");
+            Console.WriteLine($"Остаток - {pictureOst}");
+        }
+    }
+}
