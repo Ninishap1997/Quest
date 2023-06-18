e663806d5b437ab424d996564b19a9cc27df743c
александр пашинин <ninishap1997@gmail.com>
2023-06-18 18:25:10 +0300

    18

---
+++ StopWord/Program.cs
@@ -0,0 +1,33 @@
+﻿string stopWord = "Exit";
+string userInput;
+
+do
+{
+    Console.WriteLine("Введите слово");
+    userInput = Console.ReadLine();
+}
+while (userInput != stopWord);
+{
+    Console.WriteLine("Угадал");
+    Console.WriteLine("Neo, wake up");
+}
+
+userInput = null;
+
+Console.WriteLine("Так, давай еще раз, но другим способом");
+Console.WriteLine("Введите слово");
+
+while (userInput != stopWord)
+{
+    userInput = Console.ReadLine();
+    if (userInput == stopWord)
+    {
+        Console.WriteLine("Угадал");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Еще разок");
+        continue;
+    }
+}
