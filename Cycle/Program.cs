e408088a040213205d47a675e3192a05e49bcfbc
александр пашинин <ninishap1997@gmail.com>
2023-06-18 17:49:02 +0300

    18

---
+++ Cycle/Program.cs
@@ -0,0 +1,34 @@
+﻿string message;
+int repeatMessage;
+
+Console.WriteLine("Привет! Придумай текст сообщения");
+message = Console.ReadLine();
+Console.WriteLine("Сколько раз ты хочешь вопроизвести свое сообщение?");
+repeatMessage = Convert.ToInt32(Console.ReadLine());
+
+while (repeatMessage > 0)
+{
+    Console.WriteLine(message);
+    repeatMessage--;
+}
+
+
+Console.WriteLine("Попробуем другим способом");
+Console.WriteLine("Привет! Придумай текст сообщения");
+message = Console.ReadLine();
+Console.WriteLine("Сколько раз ты хочешь вопроизвести свое сообщение?");
+repeatMessage = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < repeatMessage; i++)
+{ 
+    Console.WriteLine(message); 
+}
+
+Console.WriteLine("Попробуем дополнительно разобрать твое сообщние по символам");
+Console.WriteLine("Привет! Придумай текст сообщения");
+message = Console.ReadLine();
+
+foreach (char i in message)
+{
+    Console.WriteLine(i);
+}
