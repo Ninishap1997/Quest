ae75db2d64ca6072cb2acb8afdbd735d033e2ff5
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-19 09:35:16 +0300

    19

---
+++ Enumeration/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int step = 7;
+
+            for (int i = 5; i < 100; i += step)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
