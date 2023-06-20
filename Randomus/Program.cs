78dc0704502f3e80655edf47950b33a2badce930
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-06-20 15:28:08 +0300

    20

--- Randomus/Program.cs
+++ Randomus/Program.cs
@@ -7,24 +7,27 @@ namespace Randomus
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
             int number;
-            number = random.Next(1, 101);
+            int minRandomNumber = 0;
+            int maxRandomNumber = 101;
             int startCycle = 1;
             int divider = 3;
             int sum = 0;
 
+            Random random = new Random();
+            number = random.Next(minRandomNumber, maxRandomNumber);
             Console.WriteLine($"Наше число {number}");
             Console.WriteLine("Разберем его");
 
-            for (int i = startCycle; i < number; i++)
+            for (int i = startCycle; i <= number; i++)
             {
                 if (i % divider == 0)
                 {
+                    Console.WriteLine(i);
                     sum += i;
-                    Console.WriteLine(sum);
                 }
             }
+            Console.WriteLine($"Подведем итог: Сумма чисел, числа {number} кратных {divider} равна {sum}");
         }
     }
 }
