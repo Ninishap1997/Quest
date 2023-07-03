350a8cd4c116c22cc6f0dd6d0bfa5dd127573112
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-07-03 14:38:02 +0300

    03

---
+++ Charik/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Charik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string setName;
+            string setSymbol;
+            string rectangleSymbols = "";
+
+            Console.WriteLine("Введите имя");
+            setName = Console.ReadLine();
+            Console.WriteLine("Введите символ");
+            setSymbol = Console.ReadLine();
+            
+            for (int i = 0; i < setName.Length + 2; i++)
+            {
+                rectangleSymbols += setSymbol;
+            }
+            Console.WriteLine(rectangleSymbols);
+            Console.WriteLine($"{setSymbol}{setName}{setSymbol}");
+            Console.WriteLine(rectangleSymbols);
+            Console.ReadKey();
+        }
+    }
+}
