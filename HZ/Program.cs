47ddc51ca8c4afa708c2ebbafdefeb09497c1fbe
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-07-04 16:03:13 +0300

    04

---
+++ HZ/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            int count;
+
+            Console.WriteLine("Введите число от 1 до 27");
+            while (!int.TryParse(Console.ReadLine(), out userInput) || userInput < 1 || userInput > 27)
+            {
+                Console.WriteLine("Нит, введите число от 1 до 27:");
+            }
+
+            count = 0;
+
+            for (int i = 100; i <= 999; i++)
+            {
+                if (i % userInput == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество трехзначных чисел, кратных {userInput} будет {count}");
+        }
+    }
+}
