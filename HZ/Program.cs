f66a074651543bd42f5661220c1e4c521e8e3b07
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-07-04 16:38:03 +0300

    04

--- HZ/Program.cs
+++ HZ/Program.cs
@@ -8,23 +8,28 @@
             int count;
             int minThreeDigitNumber = 100;
             int maxThreeDigitNumber = 999;
+            int minimumValue = 1;
+            int maximumValue = 27;
 
-            Console.WriteLine("Введите число от 1 до 27");
-            while (!int.TryParse(Console.ReadLine(), out userInput) || userInput < 1 || userInput > 27)
+            Console.WriteLine("Введите число от 1 до 27:");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            if (userInput > minimumValue && userInput < maximumValue)
+            {
+                count = 0;
+                for (int i = minThreeDigitNumber; i <= maxThreeDigitNumber; i++)
+                {
+                    if (i % userInput == 0)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"Количество трехзначных чисел, кратных {userInput} будет {count}");
+            }
+            else
             {
                 Console.WriteLine("Нит, введите число от 1 до 27:");
             }
-
-            count = 0;
-
-            for (int i = minThreeDigitNumber; i <= maxThreeDigitNumber; i++)
-            {
-                if (i % userInput == 0)
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine($"Количество трехзначных чисел, кратных {userInput} будет {count}");
+          
         }
     }
 }
