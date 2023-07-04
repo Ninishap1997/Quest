3ee1b22a3c46e95884ef1df09b1acbc94632dab6
Pashinin Aleksandr <Ninishap1997@gmail.com>
2023-07-04 15:35:27 +0300

    04

--- Pasword/Program.cs
+++ Pasword/Program.cs
@@ -4,11 +4,69 @@
     {
         static void Main(string[] args)
         {
-            string password;
-            string userAttempts = "5";
-            string userInput = "0";
+            const string CommandWhaaghHumanity = "1";
+            const string CommandWhaaghGorka = "2";
+            const string CommandWhaaghMorka = "3";
+            const string CommandSellPlane = "4";
 
-            for (string i = userInput;   )
+            Random random = new Random();
+            int costFang = random.Next();
+            string password = "Password";
+            string passwordInput;
+            string warBossCommand;
+            string warBossChoiseBigBird = "Штука";
+            string warBossChoiseFang = "Зубы";
+            string warBossChoise;
+            int userAttempts = 5;
+            int userInput = 0;
+
+            for (int i = userInput; userInput < userAttempts; userAttempts--)
+            {
+                Console.WriteLine("Введите пароль");
+                passwordInput = Console.ReadLine();
+
+                if (passwordInput != password)
+                {
+                    Console.WriteLine("Введен неверный пароль");
+                    Console.WriteLine($"Осталось попыток {userAttempts -1}");
+                }
+                else
+                {
+                    Console.WriteLine("Бомбовоз заправлен");
+                    Console.WriteLine("Босс, кого стукать будем?");
+                    Console.WriteLine($"Ваш приказ: {CommandSellPlane}, {CommandWhaaghGorka}, {CommandWhaaghHumanity}, {CommandWhaaghMorka}");
+                    warBossCommand = Console.ReadLine();
+
+                        switch (warBossCommand)
+                        {
+                            case CommandWhaaghHumanity:
+                                Console.WriteLine("Ми будим стукать людишей, заряжай свинцометы!");
+                                break;
+                            case CommandWhaaghGorka:
+                                Console.WriteLine("ГОРКА НЕ МОРКА ЗЕЛЕНОГОЛОВЫЙ!");
+                                break;
+                            case CommandWhaaghMorka:
+                                Console.WriteLine("НЕТ МОРКА НЕ ГОРКА!");
+                                break;
+                            case CommandSellPlane:
+                                Console.WriteLine("Большая птица или большой клык?");
+                            Console.WriteLine($"Ваш приказ: {warBossChoiseBigBird} или {warBossChoiseFang}?");
+                            warBossChoise = Console.ReadLine();
+                                
+                                if(warBossChoise == warBossChoiseBigBird)
+                                {
+                                    Console.WriteLine("Где моя тачка, я не люблю ходить пешком?");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Мне нужны их зубыы не повредите зубыыы");
+                                    Console.WriteLine($"Тупоголовый гретчин продал последний бомбовоз за {costFang} зубов");
+                                }
+                                break;
+                        }
+                    break;
+                }
+            }
         }
     }
 }
