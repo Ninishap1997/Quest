f1ab6e2ea56177c3780368806f47c270bfe080d8
александр пашинин <ninishap1997@gmail.com>
2023-07-04 19:38:16 +0300

    04

---
+++ step/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Random random = new Random();
+int randomNumber;
+int determinationDegree = 0;
+int result = 1;
+int minRundomNumber = 1;
+int maxRundomNumber = 1000;
+int degreeErection = 2;
+
+randomNumber = random.Next(minRundomNumber, maxRundomNumber);
+
+while (result <= randomNumber)
+{
+    determinationDegree++;
+    result *= degreeErection;
+}
+
+Console.WriteLine($"Число: {randomNumber}");
+Console.WriteLine($"Степень: {determinationDegree}");
+Console.WriteLine($"2 в степени {determinationDegree} = {result}");
