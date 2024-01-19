e153ff4865f1b51a7969f714fe9574b2ae453ae6
Ninishap1997 <Ninishap1997@gmail.com>
2024-01-19 22:37:09 +0300

    2237

---
+++ Kchau/Program.cs
@@ -0,0 +1,11 @@
+﻿string firstName = "Kek";
+string lastName = "Kekovich";
+string reverse;
+
+Console.WriteLine($"Ваше имя и фамилия до - {firstName} {lastName}");
+
+reverse = firstName;
+firstName = lastName;
+lastName = reverse;
+
+Console.WriteLine($"Ваше имя и фамилия после - {firstName} {lastName}");
