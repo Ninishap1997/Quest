79b98163c86b26896c9d8f24842e62e7fab4e378
Ninishap1997 <Ninishap1997@gmail.com>
2024-01-19 19:07:11 +0300

    1907

---
+++ Picture_second/Program.cs
@@ -0,0 +1,10 @@
+﻿int pictureInAlbum = 52;
+int pictureRemains;
+int pictureInScreen;
+int pictureRow = 3;
+
+pictureInScreen = pictureInAlbum / pictureRow;
+pictureRemains = pictureInAlbum % pictureRow;
+
+Console.WriteLine($"Столько рядов будет заполнено полностью {pictureInScreen}");
+Console.WriteLine($"Остаток - {pictureRemains}");
