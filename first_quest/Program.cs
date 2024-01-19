7d5266b88c2946de417b781dff007c0290d5efe2
Ninishap1997 <Ninishap1997@gmail.com>
2024-01-19 16:27:02 +0300

    16026

--- first_quest/Program.cs
+++ first_quest/Program.cs
@@ -1,22 +1,35 @@
-﻿string userInput;
-ushort scale;
-long value;
-ulong nameOfBook;
-bool isRed;
-int userOutput;
-uint temperature;
-float solvingTheEquation;
-double paragraph;
-decimal money;
-char symbolOfFreedom;
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string userInput;
+        ushort scale;
+        long value;
+        ulong nameOfBook;
+        bool isRed;
+        int userOutput;
+        uint temperature;
+        float solvingTheEquation;
+        double paragraph;
+        decimal money;
+        char symbolOfFreedom;
+        double a = 10;
+        double b = 38;
+        double c;
 
-userInput = "";
-scale = 10;
-value = -36587455656565;
-nameOfBook = 2000000;
-isRed = false;
-userOutput = -369;
-temperature = 45233;
-solvingTheEquation = 5.6f;
-money = 458632.659m;
-symbolOfFreedom = ';';
+        userInput = "";
+        scale = 10;
+        value = -36587455656565;
+        nameOfBook = 2000000;
+        isRed = false;
+        userOutput = -369;
+        temperature = 45233;
+        solvingTheEquation = 5.6f;
+        money = 458632.659m;
+        symbolOfFreedom = ';';
+
+        c = (31 - 5 * a) / b;
+        Console.WriteLine($"Ответ:{c}");
+
+    }
+}
